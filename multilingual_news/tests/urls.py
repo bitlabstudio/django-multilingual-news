b26e11992782181e3371a7718f663e881de548c9
Tobias Lorenz <tobias.lorenz@rtl.de>
2026-05-15 08:15:12 +0200

    fix: fixed urls imports

--- multilingual_news/tests/urls.py
+++ multilingual_news/tests/urls.py
@@ -4,13 +4,13 @@ As you know, every app must be hooked into yout main ``urls.py`` so that
 you can actually reach the app's views (provided it has any views, of course).
 
 """
+
 from django.conf import settings
-from django.conf.urls import include
+from django.urls import include, path
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
-from django.urls import path
 
 
 admin.autodiscover()
@@ -18,7 +18,7 @@ admin.autodiscover()
 urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += i18n_patterns(
-    path('admin/', admin.site.urls),
-    path('news/', include('multilingual_news.urls')),
-    path('', include('cms.urls')),
+    path("admin/", admin.site.urls),
+    path("news/", include("multilingual_news.urls")),
+    path("", include("cms.urls")),
 )
--- multilingual_news/urls.py
+++ multilingual_news/urls.py
@@ -1,5 +1,6 @@
 """URLs for the ``multilingual_news`` app."""
-from django.conf.urls import url
+
+from django.urls import re_path
 
 from .feeds import AuthorFeed, NewsEntriesFeed, TaggedFeed
 from .models import NewsEntry
@@ -8,48 +9,65 @@ from . import views
 
 urlpatterns = [
     # feed urls
-    url(r'^rss/any/tagged/(?P<tag>[^/]*)/$',
-        TaggedFeed(), {'any_language': True},
-        name='news_rss_any_tagged'),
-    url(r'^rss/tagged/(?P<tag>[^/]*)/$',
-        TaggedFeed(), name='news_rss_tagged'),
-    url(r'^rss/any/author/(?P<author>\d+)/$',
-        AuthorFeed(), {'any_language': True},
-        name='news_rss_any_author'),
-    url(r'^rss/author/(?P<author>\d+)/$',
-        AuthorFeed(), name='news_rss_author'),
-    url(r'^rss/any/$', NewsEntriesFeed(), {'any_language': True},
-        name='news_rss_any'),
-    url(r'^rss/$', NewsEntriesFeed(), name='news_rss'),
-
+    re_path(
+        r"^rss/any/tagged/(?P<tag>[^/]*)/$",
+        TaggedFeed(),
+        {"any_language": True},
+        name="news_rss_any_tagged",
+    ),
+    re_path(r"^rss/tagged/(?P<tag>[^/]*)/$", TaggedFeed(), name="news_rss_tagged"),
+    re_path(
+        r"^rss/any/author/(?P<author>\d+)/$",
+        AuthorFeed(),
+        {"any_language": True},
+        name="news_rss_any_author",
+    ),
+    re_path(r"^rss/author/(?P<author>\d+)/$", AuthorFeed(), name="news_rss_author"),
+    re_path(
+        r"^rss/any/$", NewsEntriesFeed(), {"any_language": True}, name="news_rss_any"
+    ),
+    re_path(r"^rss/$", NewsEntriesFeed(), name="news_rss"),
     # regular urls
-    url(r'^category/(?P<category>[^/]*)/',
+    re_path(
+        r"^category/(?P<category>[^/]*)/",
         views.CategoryListView.as_view(),
-        name='news_archive_category',),
-    url(r'^(?P<year>\d+)/(?P<month>\d+)/(?P<day>\d+)/(?P<slug>[\w-]+)/$',
+        name="news_archive_category",
+    ),
+    re_path(
+        r"^(?P<year>\d+)/(?P<month>\d+)/(?P<day>\d+)/(?P<slug>[\w-]+)/$",
         views.NewsDateDetailView.as_view(),
-        name='news_detail'),
-    url(r'^(?P<slug>[\w-]+)/$',
+        name="news_detail",
+    ),
+    re_path(
+        r"^(?P<slug>[\w-]+)/$",
         views.NewsDetailView.as_view(queryset=NewsEntry.objects.published()),
-        name='news_detail'),
-    url(r'^preview/(?P<slug>[\w-]+)/$',
+        name="news_detail",
+    ),
+    re_path(
+        r"^preview/(?P<slug>[\w-]+)/$",
         views.NewsDetailPreviewView.as_view(queryset=NewsEntry.objects.all()),
-        name='news_preview'),
-    url(r'^tag/(?P<tag>[\w-]+)$',
+        name="news_preview",
+    ),
+    re_path(
+        r"^tag/(?P<tag>[\w-]+)$",
         views.TaggedNewsListView.as_view(),
-        name='news_archive_tagged'),
-    url(r'^delete-entry/(?P<pk>\d+)/',
+        name="news_archive_tagged",
+    ),
+    re_path(
+        r"^delete-entry/(?P<pk>\d+)/",
         views.DeleteNewsEntryView.as_view(),
-        name='news_delete',),
-    url(r'^publish-entry/(?P<pk>\d+)/',
+        name="news_delete",
+    ),
+    re_path(
+        r"^publish-entry/(?P<pk>\d+)/",
         views.PublishNewsEntryView.as_view(),
-        name='news_publish',),
-    url(r'^$',
-        views.NewsListView.as_view(),
-        name='news_list'),
-
+        name="news_publish",
+    ),
+    re_path(r"^$", views.NewsListView.as_view(), name="news_list"),
     # AJAX views
-    url(r'^get-entries/',
+    re_path(
+        r"^get-entries/",
         views.GetEntriesAjaxView.as_view(),
-        name='news_get_entries',),
+        name="news_get_entries",
+    ),
 ]
